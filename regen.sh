2de7ccfeaa928be0fc4f36f37aad870b1d289d68
yurch23.88 <yurch23.88@gmail.com>
2025-05-27 09:16:10 +0000

    added some things

--- regen.sh
+++ regen.sh
@@ -7,13 +7,21 @@ echo '#!/bin/bash
 set -e
 ' > "$CAT_SCRIPT"
 
-find . -type f \
+find . \
+    -type f \
+    \( \
+        -name "*.py" -o -name "*.js" -o -name "*.jsx" -o -name "*.ts" -o -name "*.tsx" \
+        -o -name "*.css" -o -name "*.json" -o -name "*.html" -o -name "*.conf" \
+        -o -name "*.md" -o -name "*.sql" -o -name "Dockerfile" \
+        -o -name ".env" -o -name "docker-compose.yml" \
+        -o -name "install.sh" -o -name "delete.sh" \
+        -o -name "cat-all.sh" -o -name "regen.sh" \
+    \) \
     ! -path "./.git/*" \
     ! -path "./node_modules/*" \
     ! -path "./migrations/*.pyc" \
     ! -path "./migrations/__pycache__/*" \
     ! -path "./__pycache__/*" \
-    ! -name "cat-all.sh" \
     | sort | while read -r file; do
     relpath="${file#./}"
     echo "echo -e '\n========== $relpath =========='" >> $CAT_SCRIPT
